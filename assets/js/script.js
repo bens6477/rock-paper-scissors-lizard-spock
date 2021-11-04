aa7fb90f3408cc8d0162e02b3faead7403c695bc
Ben Smith <bens6477@gmail.com>
2021-11-04 19:07:59 +0000

    Add DOMContentLoaded event listener and add event listener to run checkAnswer() when button is clicked and return userSelection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,31 @@
+// Run game after DOM has finished loading
 
-let userSelection = document.getElementById('user-selection');
+document.addEventListener("DOMContentLoaded", function() {
+    // Add event listeners to buttons 
+    let buttons = document.getElementsByTagName('button');
 
-// Listen for click on user selection
-userSelection.addEventListener('click', changeImage)
+    for (let button of buttons) {
+        let userSelection = button.getAttribute("data-type");
+        button.addEventListener('click', () => checkAnswer(userSelection));
+    }
+})
+
 
 // Change image to new user selection
-function changeImage() {
-    userSelection.src = "assets/images/rock.png";
+function checkAnswer(userSelection) {
+    userImage.src = "assets/images/rock.png";
+    console.log(userSelection);
+    return userSelection
 };
 
+
+
+// Listen for click on user selection
+let userImage = document.getElementById('user-selection');
+userImage.addEventListener('click', changeImage)
+function changeImage(userSelection) {
+    userImage.src = "assets/images/rock.png";
+};
 
 
 
