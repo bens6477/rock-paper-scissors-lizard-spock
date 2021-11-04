19ac0a319638de946cc1dd464608da4225fa740a
Ben Smith <bens6477@gmail.com>
2021-11-04 17:42:15 +0000

    Add JS to change image src upon clicking user selection

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+
+let userSelection = document.getElementById('user-selection');
+
+// Listen for click on user selection
+userSelection.addEventListener('click', changeImage)
+
+// Change image to new user selection
+function changeImage() {
+    userSelection.src = "assets/images/rock.png";
+};
+
+
+
+
+
+
